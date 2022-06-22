7f923add910f6eea0ac9ad31b20792e005a76577
Centauri07 <magnus.centauri07@gmail.com>
2022-06-22 16:40:02 +0800

    added command module

---
+++ src/main/kotlin/me/centauri07/jarbapi/command/CommandModule.kt
@@ -0,0 +1,23 @@
+package me.centauri07.jarbapi.command
+
+import me.centauri07.jarbapi.BotApplication
+import me.centauri07.jarbapi.module.DiscordModule
+
+/**
+ * @author Centauri07
+ */
+class CommandModule(botApplication: BotApplication) : DiscordModule(botApplication) {
+
+    override fun onEnable() {
+
+
+
+    }
+
+    override fun onDisable() {
+
+        
+
+    }
+
+}
