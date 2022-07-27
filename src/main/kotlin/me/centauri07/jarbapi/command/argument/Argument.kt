f9a75f248e2ff59268c17e1b3bae83b2eacaa7ae
Centauri07 <magnus.centauri07@gmail.com>
2022-07-27 16:23:25 +0800

    deleted unnecessary classes because it's getting replaced with a command manager api

--- src/main/kotlin/me/centauri07/jarbapi/command/argument/Argument.kt
+++
@@ -1,8 +0,0 @@
-package me.centauri07.jarbapi.command.argument
-
-import net.dv8tion.jda.api.interactions.commands.OptionType
-
-/**
- * @author Centauri07
- */
-data class Argument(val optionType: OptionType, val name: String, val description: String, val required: Boolean)
