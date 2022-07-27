71f945d11c40b9256b2a85d513cca84525dcd977
Centauri07 <magnus.centauri07@gmail.com>
2022-07-27 19:59:12 +0800

    added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.31"
     java
+    `maven-publish`
 }
 
 group = "me.centauri07.jarbapi"
@@ -8,6 +9,8 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    
+    maven(url = uri("https://jitpack.io"))
 }
 
 dependencies {
@@ -15,7 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
 
     implementation("net.dv8tion:JDA:5.0.0-alpha.12")
-    implementation(files("C:\\Users\\Andrei\\Documents\\Programming\\Libraries and APIs\\DCM\\build\\libs\\DiscordCommandManager-v1.0-alpha.jar"))
+    implementation("com.github.Centauri07:DCM:0.0.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
@@ -24,3 +27,17 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.centauri07.jarbapi"
+            artifactId = "JarbAPI"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
+
