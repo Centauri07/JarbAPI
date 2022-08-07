b3fdfab9843c8e641f10c8ba8dca23550ad8b89b
Centauri07 <magnus.centauri07@gmail.com>
2022-08-07 18:49:41 +0800

    added interface functions

--- src/main/kotlin/me/centauri07/jarbapi/ticket/Ticket.kt
+++ src/main/kotlin/me/centauri07/jarbapi/ticket/Ticket.kt
@@ -1,7 +1,30 @@
 package me.centauri07.jarbapi.ticket
 
+import me.centauri07.jarbapi.ticket.data.TicketData
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.entities.GuildMessageChannel
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.MessageEmbed
+
 /**
  * @author Centauri07
  */
 interface Ticket {
+
+    val channel: GuildMessageChannel
+
+    fun sendMessage(message: Message)
+
+    fun sendMessage(message: MessageBuilder.() -> Unit) =
+        sendMessage(MessageBuilder().apply { message.invoke(this) }.build())
+
+    fun sendMessageEmbed(messageEmbed: MessageEmbed) = sendMessage { setEmbeds(messageEmbed) }
+    fun sendMessageEmbed(messageEmbed: EmbedBuilder.() -> Unit) =
+        sendMessageEmbed(EmbedBuilder().apply { messageEmbed.invoke(this) }.build())
+
+    fun close()
+
+    fun open()
+
 }
