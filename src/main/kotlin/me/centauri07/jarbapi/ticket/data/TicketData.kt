d343bcaab52e1cc0d2b9541ea80d21eb11e89a32
Centauri07 <magnus.centauri07@gmail.com>
2022-08-07 18:45:03 +0800

    added ticket data

---
+++ src/main/kotlin/me/centauri07/jarbapi/ticket/data/TicketData.kt
@@ -0,0 +1,7 @@
+package me.centauri07.jarbapi.ticket.data
+
+/**
+ * @author Centauri07
+ */
+class TicketData {
+}
---
+++ src/main/kotlin/me/centauri07/jarbapi/ticket/member/TicketMemberType.kt
@@ -0,0 +1,7 @@
+package me.centauri07.jarbapi.ticket.member
+
+/**
+ * @author Centauri07
+ */
+enum class TicketMemberType {
+}
