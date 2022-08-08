2d9bdb61b9a62a3293928ad249fb59a554223133
Centauri07 <magnus.centauri07@gmail.com>
2022-08-09 00:18:52 +0800

    added functions to interact with the data easily

--- src/main/kotlin/me/centauri07/jarbapi/ticket/data/TicketData.kt
+++ src/main/kotlin/me/centauri07/jarbapi/ticket/data/TicketData.kt
@@ -1,12 +1,21 @@
 package me.centauri07.jarbapi.ticket.data
 
 import me.centauri07.jarbapi.ticket.member.TicketMember
+import me.centauri07.jarbapi.ticket.member.TicketMemberType
+import org.bson.codecs.pojo.annotations.BsonId
 
 /**
  * @author Centauri07
  */
 data class TicketData(
-    val ticketId: Long,
-    val channelRef: Long,
+    @BsonId val ticketId: String,
+    val type: String,
+    var channelRef: Long,
     val members: MutableList<TicketMember>
-)
+) {
+
+    fun getOwner(): TicketMember? = members.firstOrNull { it.userType == TicketMemberType.OWNER }
+
+    fun isOwner(id: Long): Boolean = members.firstOrNull { it.memberId == id }?.userType?.equals(TicketMemberType.OWNER) ?: false
+
+}
