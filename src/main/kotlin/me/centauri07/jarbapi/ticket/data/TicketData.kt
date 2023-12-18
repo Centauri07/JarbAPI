059358d12c04cffb242b0ae84f238634e89e5776
Centauri07 <magnus.centauri07@gmail.com>
2023-12-18 22:32:02 +0800

    fix: removed error from previous dependency update

--- src/main/kotlin/me/centauri07/jarbapi/ticket/data/TicketData.kt
+++ src/main/kotlin/me/centauri07/jarbapi/ticket/data/TicketData.kt
@@ -1,14 +1,12 @@
 package me.centauri07.jarbapi.ticket.data
 
 import me.centauri07.jarbapi.ticket.member.TicketMember
-import me.centauri07.jarbapi.ticket.member.TicketMemberType
-import org.bson.codecs.pojo.annotations.BsonId
 
 /**
  * @author Centauri07
  */
 data class TicketData<T>(
-    @BsonId val ticketId: String,
+    val ticketId: String,
     val type: String,
     val guild: Long,
     var channel: Long,
