d60f06d9629c1b8fc764a09ee5fde7188c1506c4
andeng07 <sumilangandreijohn@gmail.com>
2024-05-05 19:12:00 +0800

    feat: added modal callback support

--- src/main/kotlin/me/centauri07/jarbapi/component/Callback.kt
+++ src/main/kotlin/me/centauri07/jarbapi/component/Callback.kt
@@ -1,42 +1,55 @@
 package me.centauri07.jarbapi.component
 
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.hooks.SubscribeEvent
 import net.dv8tion.jda.api.interactions.components.buttons.Button
+import net.dv8tion.jda.api.interactions.modals.Modal
 
-interface Callback<C, E> {
+open class Callback<C, E> {
 
-    fun queue(component: C, eventAction: (E) -> Unit)
+    private val queue: MutableMap<C, (E) -> Unit> = mutableMapOf()
 
-    fun dequeue(component: C, event: E)
+    fun queue(component: C, eventAction: (E) -> Unit) {
+        queue[component] = eventAction
+    }
+
+    fun dequeue(component: C, event: E) {
+        queue[component]?.let {
+            it(event)
+            queue.remove(component)
+        }
+    }
 
 }
 
 
-object ButtonCallback: Callback<String, ButtonInteractionEvent> {
-
-    private val queue: MutableMap<String, (ButtonInteractionEvent) -> Unit> = mutableMapOf()
-
-    override fun queue(component: String, eventAction: (ButtonInteractionEvent) -> Unit) {
-        queue[component] = eventAction
-    }
-
-    override fun dequeue(component: String, event: ButtonInteractionEvent) {
-        queue[component]?.let {
-            it(event)
-
-            queue.remove(component)
-        }
-    }
+object ButtonCallback: Callback<String, ButtonInteractionEvent>() {
 
     @SubscribeEvent
     fun on(e: ButtonInteractionEvent) {
         e.button.id?.let { dequeue(it, e) }
     }
+
 }
 
 fun Button.callback(eventAction: (ButtonInteractionEvent) -> Unit): Button {
     ButtonCallback.queue(id!!, eventAction)
+
+    return this
+}
+
+object ModalCallback: Callback<String, ModalInteractionEvent>() {
+
+    @SubscribeEvent
+    fun on(e: ModalInteractionEvent) {
+        dequeue(e.modalId, e)
+    }
+
+}
+
+fun Modal.callback(eventAction: (ModalInteractionEvent) -> Unit): Modal {
+    ModalCallback.queue(id, eventAction)
 
     return this
 }
