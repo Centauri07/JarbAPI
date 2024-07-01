7db688f6d4b442bf79f2fce11fd5a594b53aa0d2
andeng07 <sumilangandreijohn@gmail.com>
2024-07-01 17:51:29 +0800

    feat: added optional field functionality

--- src/main/kotlin/me/centauri07/jarbapi/form/Form.kt
+++ src/main/kotlin/me/centauri07/jarbapi/form/Form.kt
@@ -1,11 +1,14 @@
 package me.centauri07.jarbapi.form
 
+import me.centauri07.jarbapi.component.callback
 import me.centauri07.jarbapi.form.field.FormField
 import me.centauri07.jarbapi.form.field.GroupFormField
 import me.centauri07.jarbapi.module.DiscordModule
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
+import net.dv8tion.jda.api.interactions.components.ActionRow
+import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.utils.messages.MessageCreateData
 
 class Form(
@@ -19,6 +22,11 @@ class Form(
 
     companion object {
         var cancelCommand = "f!cancel"
+
+        var selectMessage: (field: FormField<*>) -> MessageCreateData =
+            {
+                MessageCreateData.fromEmbeds(EmbedBuilder().setAuthor(it.name).setDescription(it.selectMessage).build())
+            }
 
         var inquiryMessage: (field: FormField<*>) -> MessageCreateData =
             {
@@ -67,7 +75,25 @@ class Form(
             }
         }
 
-        channel.sendMessage(inquiryMessage(nextField)).queue()
+        if (nextField.required) {
+            channel.sendMessage(inquiryMessage(nextField)).queue()
+        } else {
+            channel.sendMessage(selectMessage(nextField))
+                .setActionRow(
+                    listOf(
+                        Button.success("y-${nextField.id}", "Yes").callback {
+                            nextField.required = true
+                            process()
+                            it.deferReply(true).queue()
+                        },
+                        Button.danger("n-${nextField.id}", "No").callback {
+                            nextField.acknowledged = true
+                            process()
+                            it.deferReply().queue()
+                        }
+                    )
+                ).queue()
+        }
     }
 
     fun complete() {
