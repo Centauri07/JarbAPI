4df604de79e82fed57724cf301323b78d69bb449
andeng07 <sumilangandreijohn@gmail.com>
2024-07-02 07:17:23 +0800

    refactor: changed exception. NotImplementedException -> NotImplementedError

--- src/main/kotlin/me/centauri07/jarbapi/form/field/GroupFormField.kt
+++ src/main/kotlin/me/centauri07/jarbapi/form/field/GroupFormField.kt
@@ -1,20 +1,13 @@
 package me.centauri07.jarbapi.form.field
 
-import jdk.jshell.spi.ExecutionControl.NotImplementedException
 import me.centauri07.jarbapi.form.NoFieldFoundException
 
 class GroupFormField(
     name: String, description: String, required: Boolean = true, override var value: List<FormField<*>>? = listOf()
 ) : FormField<List<FormField<*>>>(name, description, required) {
 
-    //companion object {
-    //   var inquiryMessage = "Enter %name%"
-    //}
-
-    //override var inquiryMessage: String = GroupFormField.inquiryMessage.replace("%name%", name)
-
     override fun validate(predicate: List<FormField<*>>.() -> Boolean, failureMessage: String): Boolean =
-        throw NotImplementedException("${this::class.java.name} does not support validation.")
+        throw NotImplementedError("${this::class.java.name} does not support validation.")
 
     override fun set(value: Any): Result<FormField<*>> {
         return next()?.set(value) ?: Result.failure(NoFieldFoundException())
